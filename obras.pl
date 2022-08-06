6d882d08e80a2364d04574646139e36a4a5b0c1f
Julián Koroluk <julian.koroluk@outlook.com>
2022-08-06 19:18:53 -0300

    Tarea Clase 11

--- obras.pl
+++ obras.pl
@@ -1,4 +1,4 @@
-% Tarea (13/06/22):
+% Tarea (Clases 10 y 11):
 
 % BASE DE CONOCIMIENTOS
 
@@ -104,7 +104,109 @@ esObra(Obra):- % es una Obra si fué escrita por alguien (_)
 % Si a esLibro no le agregamos el generador, no sería un predicado inversible porque el not no es inversible.
 % El generador nos "achica" el universo de opciones. */
 
-% Tarea: Una obra le gusta a Gus si es Sandman o la escribió Isaac Asimov. Tiene que ser inversible.
-% Tarea: Conviene contratar un artista si escribió un bestseller o es reincidente. Tiene que ser inversible.
-% Tarea: Queremos saber si una obra es rioplatense, que es cuando la nacionalidad de su artista es platense (Uruguay o Argentina). ¡Ojo con repetir lógica! 
-    
+% Una obra le gusta a Gus si es Sandman o la escribió Isaac Asimov. Tiene que ser inversible.
+
+leGustaAGus(sandman). % pattern matching, le pasamos directamente el átomo
+
+leGustaAGus(Obra):- 
+    escribio(isaacAsimov, Obra). 
+
+% Disyunsión lógica (ó): Se hace agregando más cláusulas al mismo predicado.
+
+% Conviene contratar un artista si escribió un bestseller o es reincidente. Tiene que ser inversible.
+
+convieneContarar(Artista):-
+    escribio(Artista, Obra),
+    esBestSeller(Obra).
+
+convieneContarar(Artista):-
+    esReincidente(Artista).
+
+% Queremos saber si una obra es rioplatense, que es cuando la nacionalidad de su artista es platense (Uruguay o Argentina). ¡Ojo con repetir lógica! 
+
+% nacionalidad(Artista, Nacionalidad). -> hecho
+nacionalidad(elsaBornemann, argentina). % expandimos nuestra base de conocimiento modelando
+nacionalidad(joseHernandez, argentina).
+nacionalidad(julioCortazar, argentina).
+nacionalidad(jorgeLuisBorges, argentina).
+nacionalidad(horacioQuiroga, uruguaya).
+nacionalidad(neilGaiman, britanica).
+nacionalidad(alanMoore, britanica).
+nacionalidad(warrenEllis, britanica).
+nacionalidad(terryPratchett, britanica).
+nacionalidad(brianAzarello, estadounidense).
+nacionalidad(frankMiller, estadounidense).
+nacionalidad(stephenKing, estadounidense).
+nacionalidad(isaacAsimov, rusa).
+
+esRioplatense(Obra) :-
+    escribio(Artista, Obra),
+    nacionalidad(Artista, Nacionalidad),
+    esNacionalidadPlatense(Nacionalidad).
+  
+  esNacionalidadPlatense(uruguaya).
+  esNacionalidadPlatense(argentina).
+
+% Para todas las obras que escribió un artista, la obra es un comic
+
+soloEscribioComics(Artista) :-
+    escribio(Artista, _), % generador para que sea inversible -> Solo ligamos el artista y dejamos libre la obra. Queremos saber para un artista (Artista) sobre todas sus obras (_)
+    forall(escribio(Artista, Obra), esComic(Obra)). % Para todo -> Cuantificador Universal
+
+soloEscribioComicsOBestSellers(Artista) :-
+  escribio(Artista, _),
+  forall(escribio(Artista, Obra), esComicOBestSeller(Obra)).
+
+esComicOBestSeller(Obra) :-
+  esComic(Obra).
+
+esComicOBestSeller(Obra) :-
+  esBestSeller(Obra).
+
+todasLasObrasSonComics :-
+  forall(escribio(_, Obra), esComic(Obra)).
+
+% Functores:
+
+/* En nuestra base de conocimientos teníamos obras, ahora conocemos los tipos de obras, que pueden ser
+novela(Genero, CantidadDeCapitulos)
+libroDeCuentos(CantidadDeCuentos)
+libroCientifico(Disciplina)
+bestSeller(Precio, CantidadDePaginas) 
+*/
+
+% (ontamos con un predicado que relaciona a cada obras con su tipo:
+
+esDeTipo(it, novela(terror, 11)).
+esDeTipo(cuentosDeLaSelva, libroDeCuentos(10)).
+esDeTipo(elUniversoEnUnaTabla, cientifico(quimica)).
+esDeTipo(elUltimoTeoremaDeFermat, cientifico(matematica)).
+esDeTipo(yoRobot, bestSeller(700, 253)).
+
+/*
+estaBuena/1 nos dice cuando una obra está buena. Esto sucede cuando:
+- Es una novela policial y tiene menos de 12 capítulos.
+- Es una novela de terror.
+- Los libros con más de 10 cuentos siempre son buenos.
+- Es una obra científica de fisicaCuantica.
+- Es un best seller y el precio por página es menor a $50.
+*/ 
+
+% Polimorfismo
+
+estaBuena(Obra):-
+    esDeTipo(Obra, Tipo), % Es polimórfica -> funciona con cualquier Tipo de obra
+    cumpleCon(Tipo). % Delegamos la repeticion de lógica
+
+cumpleCon(novela(policial, CantidadDeCapitulos)):- % Es polimorfica -> no le importa el functor que va ligado
+    CantidadDeCapitulos >= 12.
+
+cumpleCon(novela(terror, _)).
+
+cumpleCon(libroDeCuentos(CantidadDeCuentos)):-
+    CantidadDeCuentos > 10.
+
+cumpleCon(cientifico(fisicaCuantica)).
+
+cumpleCon(bestSeller(Precio, CantidadDePaginas)):-
+    Precio / CantidadDePaginas < 50.
